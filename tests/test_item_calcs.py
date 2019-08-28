1b4acc1a7718cde357943dbca907845979e47e24
Phil Robare <probare@rcnchicago.com>
2019-08-28 01:50:00 -0500

    basic selling by weight

--- tests/test_item_calcs.py
+++ tests/test_item_calcs.py
@@ -10,30 +10,33 @@ from ..receipts import POS, StockType, SaleType, Receipt
 def shop_inventory() -> Dict[str, StockType]:
     "shared test info, the inventory carried in the store."
     return {
-        "CAMP SOUP 10.75z": StockType(1.99, SaleType.EACH, StockType.standard)
+        "CAMP SOUP 10.75z": StockType(1.99, SaleType.EACH, StockType.standard),
+        "15%FAT GRD CHUCK": StockType(1.99, SaleType.BY_WT, StockType.standard),
     }
 
-def test_standard_calc_single(shop_inventory) -> None:
+
+@pytest.fixture
+def receipt(shop_inventory):
+    """ Provide a new receipt for each test """
+    pos = POS(shop_inventory)
+    return Receipt(pos)
+
+
+def test_standard_calc_single(receipt) -> None:
     """ Test that a receipt with only a single standard
         (non-discount) item on it returns the right price.
     """
-    # Create a cash register holding only a single standard item)
-    pos = POS(shop_inventory)
-    receipt = Receipt(pos)
-
     # Add an item to the receipt
     receipt += "CAMP SOUP 10.75z"
 
     # check the total
     assert receipt.total() == Decimal('1.99')
 
-def test_standard_calc_multiple(shop_inventory) -> None:
+
+def test_standard_calc_multiple(receipt) -> None:
     """ Test that a receipt with only three standard
         (non-discount) items on it returns the right total.
     """
-    # Create a cash register holding a single standard item
-    pos = POS(shop_inventory)
-    receipt = Receipt(pos)
 
     # Add items to the receipt
     receipt += "CAMP SOUP 10.75z"
@@ -42,3 +45,14 @@ def test_standard_calc_multiple(shop_inventory) -> None:
 
     # check the total
     assert receipt.total() == Decimal('1.99') * 3
+
+
+def test_standard_weighed_single(receipt) -> None:
+    """ Test that a receipt with only a single standard
+        (non-discount) weighed item on it returns the right price.
+    """
+    # Add an item to the receipt
+    receipt.add_scan("15%FAT GRD CHUCK", 3.0)
+
+    # check the total
+    assert receipt.total() == Decimal('1.99') * Decimal('3.0')
